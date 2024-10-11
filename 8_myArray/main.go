e3e90c83bd92c7c08cc9513443ea3d0a41a30061
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-11 21:24:33 +0530

    Array

---
+++ 8_myArray/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var dbz [5]string
+
+	// dbz[0] = "Goku"
+	// dbz[1] = "Vegeta"
+	// dbz[2] = "Gohan"
+	// dbz[3] = "Piccolo"
+
+	// dbz[4] = "Krillin"
+
+	// fmt.Println("The Array is: ", (dbz))
+	// fmt.Println("The Length of the Array is: ", len(dbz))
+
+
+	var dbz = [5]string{"Goku", "Vegeta", "Gohan", "Krillin"}
+	fmt.Println("The Array is: ", (dbz))
+}
