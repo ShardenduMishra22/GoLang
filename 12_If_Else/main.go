f0e9d4e15f01b0e7cdfffcdc50553f03c83058c6
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-11 23:12:13 +0530

    If Else in GoLang

---
+++ 12_If_Else/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// loginCount := 10
+
+	// var result string
+
+	// if loginCount < 10 {
+	// 	result = "Login count is less than 10"
+	// } else if loginCount == 10 {
+	// 	result = "Login count is equal to 10"
+	// } else {
+	// 	result = "Login count is greater than 10"
+	// }
+
+	// fmt.Println(result)
+
+	if num := 10; num%2 == 0 {
+		fmt.Println("Even Number")
+	} else {
+		fmt.Println("Odd Number")
+	}
+}
