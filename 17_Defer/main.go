cfb79d190daf7e1342e3b70a5b0c3769e06f3714
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-12 02:08:51 +0530

    Defer in GoLang

---
+++ 17_Defer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("World")
+	defer fmt.Println("One")
+	defer fmt.Println("Two")
+	fmt.Println("Hello")
+	myDefer()
+
+}
+
+// hello, 4, 3, 2, 1, 0, two, one, world
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Print(i)
+	}
+}
