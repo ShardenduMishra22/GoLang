f73563a12f09a545f6f5698b0fa26e64532138b8
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-13 14:23:04 +0530

    Self Made - API in GoLang

--- 24_API/main.go
+++ 24_API/main.go
@@ -23,8 +23,8 @@ func main() {
 	courses = append(
 		courses,
 		Course{
-			CourseId:   "1",
-			CourseName: "PERN Stack",
+			CourseId:    "1",
+			CourseName:  "PERN Stack",
 			CoursePrice: 499,
 			Author: &Author{
 				Fullname: "Shardendu Mishra 2",
@@ -36,8 +36,8 @@ func main() {
 	courses = append(
 		courses,
 		Course{
-			CourseId:   "2",
-			CourseName: "MERN Stack",
+			CourseId:    "2",
+			CourseName:  "MERN Stack",
 			CoursePrice: 899,
 			Author: &Author{
 				Fullname: "Shardendu Mishra 4",
@@ -57,41 +57,18 @@ func main() {
 
 	// Create, Update, Delete Courses
 	r.HandleFunc("/course", createOneCourse).Methods("POST")
+
+	// update a course
 	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+
+	// Delete a course
 	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
 
 	// Add a PATCH route for partially updating a course
 	r.HandleFunc("/course/{id}", patchOneCourse).Methods("PATCH")
 
+	// Start the server on port 4000
 	log.Fatal(http.ListenAndServe(":4000", r))
-}
-
-// Home route
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Shardendu Mishra</h1>"))
-}
-
-// Get all courses
-func getAllCourses(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get all courses")
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(courses)
-}
-
-// Get a course by ID
-func getOneCourse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get one course")
-	w.Header().Set("Content-Type", "application/json")
-
-	params := mux.Vars(r)
-
-	for _, course := range courses {
-		if course.CourseId == params["id"] {
-			json.NewEncoder(w).Encode(course)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode("No course found with the given ID")
 }
 
 // Course struct
@@ -108,16 +85,52 @@ type Author struct {
 	Website  string `json:"website"`
 }
 
-// Function to check if the course is empty
+// Function to check if the course is empty and its part of a structure
 func (c *Course) IsEmpty() bool {
 	return c.CourseName == ""
 }
 
+// Home route
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home route")
+	w.Write([]byte("<h1>Shardendu Mishra</h1>"))
+}
+
+// Get All Courses
+// r.HandleFunc("/courses", getAllCourses).Methods("GET")
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(courses)
+}
+
+// Get a course by ID
+// r.HandleFunc("/course/{id}",getOneCourse).Methods("GET")
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No course found with the given ID")
+}
+
 // Create a new course
+// r.HandleFunc("/course", createOneCourse).Methods("POST")
+
 func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Create one course")
 	w.Header().Set("Content-Type", "application/json")
 
+	// body is empty
 	if r.Body == nil {
 		json.NewEncoder(w).Encode("Please send some data")
 		return
@@ -125,23 +138,27 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	var course Course
 	err := json.NewDecoder(r.Body).Decode(&course)
-	if err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
-		return
-	}
 
+	// there is a body but nothing insde the body
 	if course.IsEmpty() {
 		json.NewEncoder(w).Encode("No data inside JSON")
 		return
 	}
 
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
 	rand.Seed(time.Now().UnixNano())
 	course.CourseId = strconv.Itoa(rand.Intn(100)) // Generate a random ID
-	courses = append(courses, course)               // Add course to the slice
-	json.NewEncoder(w).Encode(course)               // Return the created course
+	courses = append(courses, course)              // Add course to the slice
+	json.NewEncoder(w).Encode(course)              // Return the created course
 }
 
-// Update a course
+// update a course
+// r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+
 func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Update one course")
 	w.Header().Set("Content-Type", "application/json")
@@ -170,6 +187,8 @@ func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 }
 
 // Delete a course
+// r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+
 func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Delete one course")
 	w.Header().Set("Content-Type", "application/json")
@@ -178,16 +197,17 @@ func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	for index, course := range courses {
 		if course.CourseId == params["id"] {
-			courses = append(courses[:index], courses[index+1:]...) // Remove the course from the slice
+			courses = append(courses[:index], courses[index+1:]...)
 			json.NewEncoder(w).Encode("Course deleted successfully")
 			return
 		}
 	}
-
 	json.NewEncoder(w).Encode("No course found with the given ID")
 }
 
-// Patch (partially update) a course
+// Add a PATCH route for partially updating a course
+// r.HandleFunc("/course/{id}", patchOneCourse).Methods("PATCH")
+
 func patchOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Patch one course")
 	w.Header().Set("Content-Type", "application/json")
@@ -217,7 +237,7 @@ func patchOneCourse(w http.ResponseWriter, r *http.Request) {
 				course.Author = &authorData
 			}
 
-			courses[index] = course // Update the course in the slice
+			courses[index] = course           // Update the course in the slice
 			json.NewEncoder(w).Encode(course) // Return the updated course
 			return
 		}
